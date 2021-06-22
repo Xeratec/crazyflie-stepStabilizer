7083ffccc897e04b3f908316fd361ee800c4913d
Philip Wiese <philip.wiese@swissloop.ch>
2021-06-22 16:37:43 +0200

    Tried to write some useful code

--- python/capture_vicon_data.py
+++ python/capture_vicon_data.py
@@ -1,51 +1,122 @@
 import sys
 sys.path.append("../extern/pyvicon/")
 
-from datetime import datetime
-from vicon_commander import *
 import json
 import keyboard
-from numpy import arctan2, arcsin
-import simpleaudio as sa
 import time
+import simpleaudio as sa
 
-file = sys.argv[1]
-t0 = datetime.now()
+from datetime import datetime
+from threading import Thread
+from numpy import arctan2, arcsin
 
-to_log_vicon = ["Test"]
-# Variables to log from CF
-to_log_cf = ["stateEstimate.x", "stateEstimate.y", "stateEstimate.z", "stateEstimate.roll",
-          "stateEstimate.pitch", "stateEstimate.yaw"]
+from vicon_commander import *
+from cflib.positioning.motion_commander import MotionCommander
 
-vicon0 = ViconWrapper(ip="192.168.10.1", period=20, subjects=to_log_vicon, time0=t0, filename=file)
-# cflib.crtp.init_drivers(enable_debug_driver=False)
-# drones = cflib.crtp.scan_interfaces()
-# cf0 = CrazyFlieCommander(drones[0][0], log_list=to_log_cf, sampling_period=50, time0=t0, filename=file)
-# # sampling_rate: Not les than 30-40ms / variable
-# while(cf0.is_connected != True):
-#     wait = 1
+class Logger(Thread):
+    def __init__(self, filename, log_config_vicon, log_config_crazyflie, log_period_vicon=20, log_period_crazyflie=50):
+        Thread.__init__(self)
+        self.zero_time = datetime.now()
+        self.log_config_vicon = log_config_vicon
+        self.log_config_crazyflie = log_config_crazyflie
+        self.abort = False
+        self.cf = None
+        self.vicon = None
+       
+        cflib.crtp.init_drivers(enable_debug_driver=False)
+        drones = cflib.crtp.scan_interfaces()
+        print("[Logger] Found ", drones)
 
-time.sleep(1)
-print("Logging starting...")
-vicon0.logging_enabled(1)
-time.sleep(1)
-cf0.logging_enabled(1)
-vicon0.logging_enabled(1)
+        if not drones:
+            print("[Logger] No drone found!")
+            return
 
-# time.sleep(50)
-while 1:
-    time.sleep(1)
-    if keyboard.is_pressed('s'):
-        break
+        if log_config_vicon is not None:
+            self.vicon = ViconWrapper(ip="192.168.10.1", period=log_period_vicon, subjects=log_config_vicon, time0=self.zero_time, filename=filename)
+        if log_config_crazyflie is not None:
+            self.cf = CrazyFlieCommander(drones[0][0], log_list=log_config_crazyflie, sampling_period=log_period_crazyflie, time0=self.zero_time, filename=filename)
 
-# cf0.logging_enabled(0)     
-vicon0.logging_enabled(0)
-vicon0.save_log()
-# cf0.save_log()
-print("Logging finished!")
+        self.start()
+
+    def run(self):
+        while(self.cf.is_connected != True):
+            pass
+
+        time.sleep(1)
+        print("Logging starting...")
+        time.sleep(1)
+        if self.log_config_vicon is not None:
+            self.vicon.logging_enabled(1)
+
+        if self.log_config_crazyflie is not None:
+            self.cf.logging_enabled(1)
+
+            self.mc = MotionCommander(self.cf.scf)
+            with self.mc as mc:
+
+                print('Taking off!')
+                mc.up(0.7)
+                time.sleep(1)
+
+                print('Moving up 0.2m')
+                mc.up(0.7)
+                # Wait a bit
+                time.sleep(1)
+
+                # There is a set of functions that move a specific distance
+                # We can move in all directions
+                print('Moving forward 0.5m')
+                
+                mc.forward(0.5)
+                # Wait a bit
+                time.sleep(10)
+
+                # We land when the MotionCommander goes out of scope
+                print('Landing!')
+
+        while self.abort == False:
+            pass
+
+        self.stop()
+
+        filename = 'beep.wav'
+        wave_obj = sa.WaveObject.from_wave_file(filename)
+        play_obj = wave_obj.play()
+        play_obj.wait_done()  # Wa
+
+    def stop(self):
+        self.abort = True
+        if self.log_config_vicon is not None:
+            self.vicon.logging_enabled(0)
+            self.vicon.save_log()
+        if self.log_config_crazyflie is not None and self.cf is not None:
+            self.cf.logging_enabled(0)     
+            self.cf.save_log()
+            print('Landing!')
+            self.mc.land()
 
 
-filename = 'beep.wav'
-wave_obj = sa.WaveObject.from_wave_file(filename)
-play_obj = wave_obj.play()
-play_obj.wait_done()  # Wait until sound has finished playing
+if __name__ == '__main__':
+    log_config_vicon = None
+    # Variables to log from CF
+    log_config_crazyflie = ["stateEstimate.x", "stateEstimate.y", "stateEstimate.z", "stateEstimate.roll",
+                    "stateEstimate.pitch", "stateEstimate.yaw"]
+
+     ## run function in the background
+    logger = Logger(filename=sys.argv[1], log_config_vicon=log_config_vicon, log_config_crazyflie=log_config_crazyflie)
+    
+    ## will not exit if function finishes, only when
+    ## "q" is entered, but this is just a simple example
+    while True:
+        if keyboard.is_pressed('q'):
+            break
+   
+    print("Aborting...")
+    logger.stop()
+
+
+
+
+   
+
+   
